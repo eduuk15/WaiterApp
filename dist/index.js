a21a77ec5a67f1776ce56b3366180ea6b358ed67
eduuk15 <98836125+eduuk15@users.noreply.github.com>
2022-11-17 15:55:42 -0300

    feat: adicionaas rotas http do app

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,11 @@
-"use strict";
+'use strict';
 var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
+	return (mod && mod.__esModule) ? mod : { 'default': mod };
 };
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
+Object.defineProperty(exports, '__esModule', { value: true });
+const express_1 = __importDefault(require('express'));
 const app = (0, express_1.default)();
 const port = 3001;
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+	console.log(`Server is running on http://localhost:${port}`);
 });
